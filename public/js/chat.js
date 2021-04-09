41621b3cc7264afb853f12a5036fa823d5b53aa9
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-04-09 22:55:20 +0545

    work on the ui of the location send buttons and send message buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,54 @@
 //cliet side function return value
 const socket = io()
+
+//elements of the forms
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+//elements of the button of the location
+const $sendLocationButton = document.querySelector('#send-location')
+
 //message is the event name and second parameter is the callback function
 socket.on('message',(msg)=>{
     console.log(msg)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+$messageForm.addEventListener('submit', (e)=>{
     //prevent full page refresh
     e.preventDefault()
-    //get the value form the input field from the form e is the event from there target and from there name attribute which is a fullproof.
+    //get the value form the input field from the form e is the event from there target and from there name attribute which is a fullproof method to get the value.
+    //disabled the form button
+    $messageFormButton.setAttribute('disabled','disabled')
     const message = e.target.elements.message.value
-    
-    socket.emit('sendMessage', message)
+
+    //final argument as an callback function for the event acknowledgement
+    socket.emit('sendMessage', message, (error) => {
+
+        //removing the attribute name disabled of the form button
+        $messageFormButton.removeAttribute('disabled')
+        //clearing the input value
+        $messageFormInput.value = ''
+        //getting the focus in the input
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('message is delivered!')
+    })
     
 })
 
 //This is the event listener used specifically for the finding the geolocation of the user
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
 //Some of the browser don't support the navigator feature so to address that we need to make sure we deal with that with if else statement
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
     }
+
+    $sendLocationButton.setAttribute('disabled','disabled')
 //the navigator is used to fetch the current location data of the user.
     navigator.geolocation.getCurrentPosition((position)=>{
         
@@ -28,6 +56,9 @@ document.querySelector('#send-location').addEventListener('click',()=>{
         socket.emit('sendLocation',  {
             lati: position.coords.latitude,
             longi: position.coords.longitude
+        },()=>{
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('location shared!')
         })
     })
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ const http = require('http')
 const express = require('express')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
-//The variable server is just used for creating raw http server. which is really importing for passing to the socketio function
+//The variable server is just used for creating raw http server. which is really important for passing to the socketio function
 const server = http.createServer(app)
 //io is new instance of socket.io where we call socketio as the function and pass the server
-//now the server supports websocket
+//now the new server supports websocket
 const io = socketio(server)
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
+
 //It helps to join the public folder which contain public files to join with this current src folder
 const publicDirectoryPath = path.join(__dirname, '../public')//'../ helps to go in the root folder'
 
@@ -28,16 +30,30 @@ io.on('connection', ( socket )=>{
 //send event server custom one
 //message is the event name and greet is the data to be send
     socket.emit('message', greet)
+
 //send it to everybody except the one who joined the connection(particular connection)
     socket.broadcast.emit('message','New user has joined')
 
-    socket.on('sendMessage', (message)=>{
-        io.emit('message', message)
-    })
+//callback is used for the acknowledgement, we can use this callback to send message to the client
+    socket.on('sendMessage', (message, callback)=>{
 
-    socket.on('sendLocation', (pos)=>{
+//used to filter the words containing foul language
+        const filter = new Filter()
+        //checking the message data that contain the foul language
+        if (filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
+
+        io.emit('message', message)
+//passing the message to the client as an acknowledgement
+        callback()
+       
+    })
+//callback parameter has been acknowledgement
+    socket.on('sendLocation', (pos, callback)=>{
         //receiving the information from the sendlocation event and displaying it
         io.emit('message', `https://google.com/maps?q=${pos.lati},${pos.longi}`)
+        callback()
     })
 
 //built in event 'disconnect' and send the message to the all the user in that connection
